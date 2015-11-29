5db42ab822828e09b4591b586139a1b66c8d6117
Erik Carlsson <info@code301.com>
2015-11-29 19:14:33 +0000

    Dev (not working)

--- lib/App/PongMania.pm
+++ lib/App/PongMania.pm
@@ -1,10 +1,87 @@
+use 5.20.0;
 use strict;
 use warnings;
 
-package App::PongMania;
+package App::PongMania {
+    
+    use Mojo::Base 'Mojolicious';
+    use Mojo::Util 'steady_time';
+    use Mojo::Base 'Mojolicious::Plugin';
+    use File::ShareDir::Tarball 'dist_dir';
+    use Path::Tiny;
+    use Data::Dump::Streamer;
+    use Safe::Isa;
+    use experimental qw/signatures postderef/;
 
-# VERSION
-# ABSTRACT: Short intro
+    # VERSION
+    # ABSTRACT: ..
+
+    sub startup($self) {
+        $self->setup_directories;
+        $self->setup_helpers;
+        $self->setup_hooks;
+        $self->setup_routes;
+    }
+
+    sub setup_routes($self) {
+        my $r = $self->routes;
+
+        $r->get('/')->to(cb => sub ($c) { $c->reply->static('index.html'); });
+
+    }
+
+    sub setup_helpers($self) {
+        $self->secrets(['asgsdgdrgrstfasdfasf']);
+        $self->defaults(layout => 'default');
+        $self->plugin('UnicodeNormalize');
+        $self->plugin(BootstrapHelpers => {
+            icons => {
+                class => 'glyphicon',
+                formatter => 'glyphicon-%s',
+            },
+        });
+    }
+
+    sub setup_hooks($self) {
+        $self->hook(around_action => sub {
+            my($next, $c, $action, $last) = @_;
+
+            my $starttime = steady_time;
+            $next->();
+            $self->log->debug(sprintf 'Time to render <%s>: %f for %s using %s', $c->req->url, steady_time - $starttime, $c->tx->remote_address, $c->req->headers->user_agent);
+        });
+    }
+
+    sub setup_directories($self) {
+
+        # add our template directory
+        if(path(qw/share templates/)->exists) {
+            $self->renderer->paths([path(qw/share templates/)->realpath]);
+        }
+        else {
+            my $template_dir = path(dist_dir('App-PongMania'))->child(qw/templates/);
+
+            if($template_dir->is_dir) {
+                $self->renderer->paths([$template_dir->realpath]);
+            }
+        }
+
+        # add static directory
+        if(path(qw/share public/)->exists) {
+            $self->static->paths([path(qw/share public/)->realpath]);
+        }
+        else {
+            my $public_dir = path(dist_dir('App-PongMania'))->child('public');
+
+            if($public_dir->is_dir) {
+                $self->static->paths([$public_dir->realpath]);
+            }
+        }
+
+    }
+}
+
+1;
 
 
 
---
+++ share/ko/src/app/pongmania.js
@@ -0,0 +1,10 @@
+//export class Pong {
+//    constructor(params) {
+//        console.log(params);
+//        console.log(params.elementId);
+//        var gameArea = document.getElementById(params.elementId);
+//        this.width = gameArea.width;
+//        console.log(this.width);
+//    }
+//}
+//
