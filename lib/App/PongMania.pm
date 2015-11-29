12c576e709461a13a552eab2bb66f7be43b8f970
Erik Carlsson <info@code301.com>
2015-11-29 12:40:38 +0000

    Init

---
+++ lib/App/PongMania.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package App::PongMania;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use App::PongMania;
+
+=head1 DESCRIPTION
+
+App::PongMania is ...
+
+=head1 SEE ALSO
+
+=cut
